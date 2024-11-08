bfe06dc5c19d7149857db6317ab30ad96116ebf6
Nemhesis-Idrissa <43876545+Nemhesis-Idrissa@users.noreply.github.com>
2024-11-08 21:50:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.my-button').addEventListener('click', function() {
+        location.href = 'https://www.leagueoflegends.com/fr-fr/';
+    });
+});
